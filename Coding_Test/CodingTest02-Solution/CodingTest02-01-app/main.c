616565306f93d2b42e9e85a20c978ae1f1737fff
Haluey <cheong7394wol@gmail.com>
2026-02-19 17:26:44 +0900

    토이 프로젝트 완료

---
+++ Coding_Test/CodingTest02-Solution/CodingTest02-01-app/main.c
@@ -0,0 +1,10 @@
+﻿#include <stdio.h>
+
+int main(void) {
+	int a = 5;
+	double b = 2.0;
+
+	printf("%d\n", a + b);
+
+	return 0;
+}
---
+++ Coding_Test/CodingTest02-Solution/CodingTest02-04-app/main.c
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+
+void change(int* p) {
+	*p = 50;
+}
+
+int main(void) {
+	int a = 10;
+	
+	change(&a);
+
+	printf("%d\n", a);
+
+	return 0;
+}
